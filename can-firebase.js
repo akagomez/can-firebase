aa1436c59644577442b9180755e3728c9c8f13e1
Chris Gomez <akagomez@gmail.com>
2016-04-25 14:04:58 -0400

    Define global; Reset version

--- can-firebase.js
+++ can-firebase.js
@@ -1,5 +1,12 @@
 import Model from './model/';
 
-export default {
+var firebasePlugin = {
   Model
 };
+
+// Register the Firebase plugin to the `can` namespace
+if (typeof window !== 'undefined' && !require.resolve && window.can) {
+  window.can.Firebase = firebasePlugin;
+}
+
+export default firebasePlugin;
