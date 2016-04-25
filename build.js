2281a530396412595147223b0e0eb368f9169da0
Chris Gomez <akagomez@gmail.com>
2016-04-25 13:41:33 -0400

    Enable npm test, build, release

---
+++ build.js
@@ -0,0 +1,17 @@
+var stealTools = require("steal-tools");
+
+stealTools.export({
+  system: {
+    config: __dirname + "/package.json!npm"
+  },
+  outputs: {
+    "+amd": {},
+    "+global-js": {},
+    "+cjs": {}
+  }
+}).catch(function (e){
+  setTimeout(function (){
+    throw e;
+  }, 1);
+});
+
